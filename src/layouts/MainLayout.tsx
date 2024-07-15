fb1e413e4d663b5f294dbc403f2e500e846f644e
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-15 11:36:04 -0300

    Adding the header and its styles

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function MainLayout() {
   return (
     <>
+      <header>
+        <h1>Kanban Board</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="">My board</NavLink>
+            </li>
+            <li>
+              <NavLink to="">About us</NavLink>
+            </li>
+            <li>
+              <NavLink to="">Login</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Outlet></Outlet>
     </>
   );
