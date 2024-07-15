6e9c32064c138716bac8e91ba18266018889cea7
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-15 17:11:35 -0300

    Adding about us page

---
+++ src/pages/AboutUsPage.tsx
@@ -0,0 +1,38 @@
+export default function AboutUsPage() {
+  return (
+    <div className="about-us">
+      <h2>About us</h2>
+      <p>
+        Welcome to Kanban Board, your go-to platform for efficient task
+        management and productivity. Designed with simplicity in mind, our
+        intuitive interface helps individuals stay organized and focused on
+        their goals.
+      </p>
+
+      <p>
+        Kanban Board allows you to create personalized boards tailored to your
+        workflow. Whether you're managing personal projects, tracking
+        assignments, or handling daily tasks, our flexible boards adapt to your
+        needs seamlessly.
+      </p>
+
+      <p>
+        Effortlessly add tasks, set priorities, and track progress in real-time.
+        Our platform ensures your workflow remains streamlined without the
+        complexities of team collaboration. It's designed for individuals who
+        value a straightforward approach to task management.
+      </p>
+
+      <p>
+        Rest assured, your data is secure with us. We prioritize your privacy by
+        encrypting and safeguarding your information.
+      </p>
+
+      <p>
+        Join countless users who rely on Kanban Board to enhance their
+        productivity and achieve their objectives. Experience the simplicity of
+        efficient task management today.
+      </p>
+    </div>
+  );
+}
