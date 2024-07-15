53426f4ee1fb6d7437400f18865afac5cec78064
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-15 14:20:00 -0300

    Adding form and styling login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div className="login"></div>;
+  const [registerMode, useRegisterMode] = useState<boolean>(false);
+
+  return (
+    <div className="login">
+      <form>
+        <label>Email</label>
+        <input type="email" placeholder="example@exg.com" />
+
+        <label>Password</label>
+        <input type="password" placeholder="*********" />
+
+        <button>{registerMode ? "Register" : "Log In"}</button>
+      </form>
+
+      <div className="info">
+        {!registerMode ? (
+          <>
+            <p>Don't you have an account?</p>
+            <button onClick={() => useRegisterMode(!registerMode)}>
+              Register
+            </button>
+          </>
+        ) : (
+          <>
+            <p>Do you already have an account?</p>
+            <button onClick={() => useRegisterMode(!registerMode)}>
+              Login
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
