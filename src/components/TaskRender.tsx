c5f1cf2a7507bd0ca4576e20391e68a53c27fd4c
LucasGitHubx <lucasdarosa450@gmail.com>
2024-07-16 14:08:21 -0300

    I created a new component called task render, this will be the one responsible for rendering the tasks.

--- src/components/TaskRender.tsx
+++ src/components/TaskRender.tsx
@@ -1,28 +1,96 @@
 import { Task } from "../types";
 
+// STORES
+import { useTaskStore } from "../store/useTaskStore";
+import { useInProgressTaskStore } from "../store/useTaskStore";
+import { useDoneTaskStore } from "../store/useTaskStore";
+
+// FIRESTORE
+import { deleteNormalTask } from "../firebase/firestore";
+import { deleteInProgressTask } from "../firebase/firestore";
+import { deleteDoneTask } from "../firebase/firestore";
+import { addInProgressTask } from "../firebase/firestore";
+import { addDoneTask } from "../firebase/firestore";
+
 interface Props {
   task: Task;
   state: string;
+  email: string | null | undefined;
 }
 
-export default function TaskRender({ task, state }: Props) {
+export default function TaskRender({ task, state, email }: Props) {
+  const { setTasks, setLoaded } = useTaskStore((state) => ({
+    setTasks: state.setTask,
+    setLoaded: state.setLoaded,
+  }));
+
+  const { setInProgressTasks, setInProgressLoaded } = useInProgressTaskStore(
+    (state) => ({
+      setInProgressTasks: state.setTask,
+      setInProgressLoaded: state.setLoaded,
+    })
+  );
+
+  const { setDoneTasks, setDoneLoaded } = useDoneTaskStore((state) => ({
+    setDoneTasks: state.setTask,
+    setDoneLoaded: state.setLoaded,
+  }));
+
+  function handleDeleteButton(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    e.preventDefault();
+
+    if (state == "normal" && task.taskID !== undefined) {
+      deleteNormalTask(task.taskID, setTasks, setLoaded, email);
+    } else if (state == "in-progress" && task.taskID !== undefined) {
+      deleteInProgressTask(
+        task.taskID,
+        setInProgressTasks,
+        setInProgressLoaded,
+        email
+      );
+    } else if (state == "done" && task.taskID !== undefined) {
+      deleteDoneTask(task.taskID, setDoneTasks, setDoneLoaded, email);
+    }
+  }
+
+  function handleAddInProgress() {
+    addInProgressTask(task, setInProgressTasks, setInProgressLoaded);
+    deleteNormalTask(task.taskID, setTasks, setDoneLoaded, email);
+  }
+
+  function handleAddDone() {
+    addDoneTask(task, setDoneTasks, setDoneLoaded);
+    deleteInProgressTask(
+      task.taskID,
+      setInProgressTasks,
+      setInProgressLoaded,
+      email
+    );
+  }
+
   return (
     <div className="task">
       <div className="text">
-        <p>
-          {task.taskName} {state}
-        </p>
+        <p>{task.taskName}</p>
       </div>
 
       <div className={state == "done" ? "buttons done" : "buttons"}>
-        <button className="delete">delete</button>
+        <button className="delete" onClick={handleDeleteButton}>
+          delete
+        </button>
 
         {state == "normal" ? (
-          <button className="in-progress">In progress</button>
+          <button className="in-progress" onClick={handleAddInProgress}>
+            In progress
+          </button>
         ) : undefined}
 
         {state == "in-progress" ? (
-          <button className="done">Done</button>
+          <button className="done" onClick={handleAddDone}>
+            Done
+          </button>
         ) : undefined}
       </div>
     </div>
